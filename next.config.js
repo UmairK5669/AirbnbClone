9d98771ec24f90d880cdbd3e61bfee4bb67b0271
Umair Khan <umair@Umairs-MacBook-Pro.local>
2024-05-03 16:27:02 -0400

    updated config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/airbnbclone",
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
     experimental: {
         appDir: true,
     },
